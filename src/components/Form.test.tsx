64acb87b8d947c4b2a1fc9b800f2595762b49046
gaelMon <gael.monbrault@ovianet.com>
2022-02-03 15:26:22 +0100

    delete old files

--- src/components/Form.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Form from "./Form";
-
-describe("Form input text", () => {
-  test("if input is a string", () => {
-    const inputText = "";
-  });
-});
