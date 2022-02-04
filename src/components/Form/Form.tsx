d3b29668ccb50340d6fbfca7cf8d90d70db4fab0
gaelMon <gael.monbrault@ovianet.com>
2022-02-04 16:22:14 +0100

    add new properties to form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,16 +1,28 @@
-import { userInfo } from "os";
 import { useReducer } from "react";
+import "./Form.css";
 
-type State = {
-  counterStep: number;
-  user: {
-    id: number;
-    firstname: string;
-    lastname: string;
-    email: string;
-  };
+/* TYPE USER */
+type User = {
+  id: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+  birthday: Date;
+  city: string;
+  postalCode: number;
+  address: string;
+  phoneNumber: number;
+  supInfos: string;
 };
 
+/* STATE */
+type State = {
+  counterStep: number;
+  user: User;
+  isSend: boolean;
+};
+
+/* INITIAL TYPE */
 const initialState: State = {
   counterStep: 1,
   user: {
@@ -18,33 +30,106 @@ const initialState: State = {
     firstname: "",
     lastname: "",
     email: "",
+    birthday: new Date(),
+    city: "",
+    postalCode: 0,
+    address: "",
+    phoneNumber: 0,
+    supInfos: "",
   },
+  isSend: false,
 };
 
+/* GLOBAL CONST */
 const STEPFORM = "STEPFORM";
+const FIRSTNAME = "FIRSTNAME";
+const LASTNAME = "LASTNAME";
+const EMAIL = "EMAIL";
+const BIRTHDAY = "BIRTHDAY";
+const CITY = "CITY";
+const POSTALCODE = "POSTALCODE";
+const ADDRESS = "ADDRESS";
+const PHONENUMBER = "PHONENUMBER";
+const SUPINFOS = "SUPINFOS";
 const SENDFORM = "SENDFORM";
 
+/* ACTION TYPE */
 type ACTIONTYPE =
   | { type: typeof STEPFORM }
+  | { type: typeof FIRSTNAME; payload: { firstname: string } }
+  | { type: typeof LASTNAME; payload: { lastname: string } }
+  | { type: typeof EMAIL; payload: { email: string } }
+  | { type: typeof BIRTHDAY; payload: { birthday: Date } }
+  | { type: typeof CITY; payload: { city: string } }
+  | { type: typeof POSTALCODE; payload: { postalCode: number } }
+  | { type: typeof ADDRESS; payload: { address: string } }
+  | { type: typeof PHONENUMBER; payload: { phoneNumber: number } }
+  | { type: typeof SUPINFOS; payload: { supInfos: string } }
   | { type: typeof SENDFORM; payload: {} };
 
+/* REDUCER */
 const reducer = (state = initialState, action: ACTIONTYPE) => {
   switch (action.type) {
     case STEPFORM:
-      console.log({ state, action });
       return {
         ...state,
         counterStep: state.counterStep + 1,
       };
-    case SENDFORM:
-      console.log("user profil : ", state.user);
-      state.user.id++;
+    case FIRSTNAME:
+      state.user.firstname = action.payload.firstname;
       return {
         ...state,
+      };
+    case LASTNAME:
+      state.user.lastname = action.payload.lastname;
+      return {
+        ...state,
+      };
+    case EMAIL:
+      state.user.email = action.payload.email;
+      return {
+        ...state,
+      };
+    case BIRTHDAY:
+      state.user.birthday = action.payload.birthday;
+      return {
+        ...state,
+      };
+    case CITY:
+      state.user.city = action.payload.city;
+      return {
+        ...state,
+      };
+    case POSTALCODE:
+      state.user.postalCode = action.payload.postalCode;
+      return {
+        ...state,
+      };
+    case ADDRESS:
+      state.user.address = action.payload.address;
+      return {
+        ...state,
+      };
+    case PHONENUMBER:
+      state.user.phoneNumber = action.payload.phoneNumber;
+      return {
+        ...state,
+      };
+    case SUPINFOS:
+      state.user.supInfos = action.payload.supInfos;
+      return {
+        ...state,
+      };
+    case SENDFORM:
+      state.user.id++;
+      state.isSend = true;
+      return {
+        ...state,
+        isSend: state.isSend,
         user: state.user,
       };
     default:
-      return state;
+      throw new Error("checkout an error in form !");
   }
 };
 
@@ -53,16 +138,20 @@ const Form = () => {
 
   return (
     <div className="container">
-      <div className="flex flex-col md:flex-row outline outline-purple-500 rounded p-3 w-4/5 m-auto">
+      <div
+        className={`flex-col md:flex-row outline outline-purple-500 rounded p-3 w-4/5 m-auto ${
+          state.isSend ? "hidden" : "flex"
+        }`}
+      >
         {/* STEP FORM */}
         <div className="basis-1/3">
           <ul className="steps steps-vertical">
-            <li className="step step-primary">Register</li>
+            <li className="step step-primary">Identité</li>
             <li className={`step ${state.counterStep > 1 && "step-primary"}`}>
-              Purchase
+              Lieu de résidence
             </li>
             <li className={`step ${state.counterStep > 2 && "step-primary"}`}>
-              Purchase
+              Informations complémentaire
             </li>
           </ul>
         </div>
@@ -72,8 +161,8 @@ const Form = () => {
           <form id="form-step-one" className="flex flex-col">
             {/* FIRST STEP FORM */}
             <div
-              className={`flex flex-col rounded ${
-                state.counterStep > 1 ? "invisible" : "visible"
+              className={`flex-col rounded ${
+                state.counterStep > 1 ? "hidden" : "flex"
               }`}
             >
               {/* FIRSTNAME - LASTNAME */}
@@ -82,16 +171,37 @@ const Form = () => {
                   <label className="label">
                     <span className="label-text">Firstname</span>
                   </label>
-                  <input type="text" placeholder="Jean" className="input" />
+                  <input
+                    type="text"
+                    placeholder="Jean"
+                    className="input"
+                    value={state.user.firstname}
+                    onChange={(e) =>
+                      dispatch({
+                        type: FIRSTNAME,
+                        payload: { firstname: e.target.value },
+                      })
+                    }
+                  />
                 </div>
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Lastname</span>
                   </label>
-                  <input type="text" placeholder="Dubois" className="input" />
+                  <input
+                    type="text"
+                    placeholder="Dubois"
+                    className="input"
+                    value={state.user.lastname}
+                    onChange={(e) =>
+                      dispatch({
+                        type: LASTNAME,
+                        payload: { lastname: e.target.value },
+                      })
+                    }
+                  />
                 </div>
               </div>
-
               {/* EMAIL */}
               <div className="form-control">
                 <label className="label">
@@ -103,16 +213,96 @@ const Form = () => {
                   className="input"
                 />
               </div>
+              {/* BIRTHDAY DATE */}
+              <div className="form-control">
+                <label htmlFor="birthdate" className="label">
+                  <span className="label-text">Année de naissance</span>
+                </label>
+                <input type="date" name="birthdate" className="input" />
+              </div>
             </div>
 
             {/* SECOND STEP FORM */}
+            <div
+              className={`flex-col rounded ${
+                state.counterStep === 2 ? "flex" : "hidden"
+              }`}
+            >
+              {/* ADDRESS */}
+              <div className="form-control">
+                <label htmlFor="address" className="label">
+                  <span className="label-text">Adresse</span>
+                </label>
+                <input
+                  type="text"
+                  name="address"
+                  className="input"
+                  placeholder="1 rue des lilas"
+                />
+              </div>
+              <div className="flex flex-col md:flex-row justify-between">
+                {/* POSTAL CODE */}
+                <div className="form-control">
+                  <label htmlFor="postalCode" className="label">
+                    <span className="label-text">Code postal</span>
+                  </label>
+                  <input
+                    name="postalCode"
+                    type="number"
+                    placeholder="35000"
+                    className="input"
+                  />
+                </div>
+                {/* CITY */}
+                <div className="form-control">
+                  <label htmlFor="city" className="label">
+                    <span className="label-text">Ville</span>
+                  </label>
+                  <input
+                    name="city"
+                    type="text"
+                    placeholder="Rennes"
+                    className="input"
+                  />
+                </div>
+                {/* PHONE NUMBER */}
+                <div className="form-control">
+                  <label htmlFor="phoneNumber" className="label">
+                    <span className="label-text">Téléphone</span>
+                  </label>
+                  <input
+                    name="phoneNumber"
+                    type="number"
+                    placeholder="01 10 20 30 40"
+                    className="input"
+                  />
+                </div>
+              </div>
+            </div>
 
             {/* THIRD STEP FORM */}
+            <div
+              className={`flex-col rounded 
+              ${state.counterStep === 3 ? "flex" : "hidden"}`}
+            >
+              <div className="form-control">
+                <label htmlFor="informations" className="label">
+                  <span className="label-text">
+                    Informations complémentaires
+                  </span>
+                </label>
+                <textarea
+                  name="informations"
+                  className="textarea h-24"
+                  placeholder="lorem ipsum ..."
+                ></textarea>
+              </div>
+            </div>
 
             {/* STEPS BTNS */}
             <button
               className={`btn btn-circle btn-md bg-purple-800 hover:bg-purple-600 mt-5 self-end ${
-                state.counterStep > 2 ? "invisible" : "visible"
+                state.counterStep > 2 ? "hidden" : "block"
               }`}
               type="button"
               onClick={() => dispatch({ type: STEPFORM })}
@@ -135,14 +325,30 @@ const Form = () => {
             {/* ENDING BTN FORM */}
             <button
               type="button"
-              className={`btn rounded btn-md bg-purple-800 hover:bg-purple-600 self-end ${
-                state.counterStep > 2 ? "visible" : "invisible"
+              className={`btn rounded btn-md bg-purple-800 hover:bg-purple-600 mt-5 self-end ${
+                state.counterStep > 2 ? "block" : "hidden"
               }`}
               onClick={() => dispatch({ type: SENDFORM, payload: {} })}
             >
               Envoyer
             </button>
           </form>
+        </div>
+      </div>
+
+      {/* RENDERING PROFIL */}
+      <div
+        className={`flex-col md:flex-row outline outline-purple-900 rounded p-3 w-4/5 m-auto profil-user-list ${
+          state.isSend ? "flex" : "hidden"
+        }`}
+      >
+        <div className="flex">
+          <div className="avatar placeholder">
+            <div className="bg-neutral-focus text-neutral-content rounded-full w-16 h-16">
+              <span>MX</span>
+            </div>
+          </div>
+          <p>Prénom : {state.user.firstname}</p>
         </div>
       </div>
     </div>
